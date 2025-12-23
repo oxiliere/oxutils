3a4306db9888fe05c4e421ac9fe79a7660fe7588
Edimedia Mutoke <eddycondor07@gmail.com>
2025-12-23 23:26:40 +0200

    add: token user model

--- src/oxutils/jwt/models.py
+++ src/oxutils/jwt/models.py
@@ -1,4 +1,8 @@
-# TokenTenant model
+from uuid import UUID
+from django.utils.functional import cached_property
+from ninja_jwt.models import TokenUser as DefaultTonkenUser
+from ninja_jwt.settings import api_settings
+
 import structlog
 from .tokens import OrganizationAccessToken
 
@@ -48,3 +52,17 @@ class TokenTenant:
         except Exception:
             logger.exception('Failed to create TokenTenant from token', token=token)
             return None
+
+
+class TokenUser(DefaultTonkenUser):
+    @cached_property
+    def id(self):
+        return UUID(self.token[api_settings.USER_ID_CLAIM])
+
+    @cached_property
+    def token_created_at(self):
+        return self.token.get('cat', None)
+
+    @cached_property
+    def token_session(self):
+        return self.token.get('session', None)
